6782b5de1b49f0edd3b32c08fb1c0dac8b104b40
Danubia Camargo <danubiabcamargo@gmail.com>
2023-09-16 18:58:01 -0300

    primeira att

---
+++ main.js
@@ -0,0 +1,13 @@
+function tocaSomPom(){
+  document.querySelector('#som_tecla_pom').play();
+}
+
+/*
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+O ponto vai entrar para dentro daquele elemento que está sendo buscado no querySelector e vai trazer todas propriedades e funcionalidades que são possíveis ser consultadas ou executadas dentro daquele elemento. Então com um ponto o próprio Visual Studio Code abriu uma janelinha de coisas que são possíveis fazer.
+
+E o que queremos aqui agora é acessar o atributo onclick desse elemento, que sabemos que existe porque está lá no HTML, se podemos usar ele no HTML, também podemos usar ele aqui no JavaScript. Então eu vou escrever onclick. Escrevendo onclick eu vou ter acesso a esse atributo e como ele é um atributo, para eu passar e definir um valor para ele, eu uso o sinal de atribuição, o mesmo do HTML, que é o sinal de igual =.*/
+
+//lista de elementos com selectorAll
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
